f47420a107bafe9d3f1699dbac2906c5d563271c
Varun Harinath <varunharinath15@gmail.com>
2024-06-13 19:52:25 +0530

    Compoents:Created

---
+++ src/Components/AdminPannel/AdminPannelComponents/EventDetailsChange.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EventDetailsChange = () => {
+  return <div>EventDetailsChange</div>;
+};
+
+export default EventDetailsChange;
---
+++ src/Components/AdminPannel/AdminPannelComponents/MailParticipants.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MailParticipants = () => {
+  return <div>MailParticipants</div>;
+};
+
+export default MailParticipants;
